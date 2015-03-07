d3a2637b92da2f36334ffbea14ef6f14388bfc2d
ryanhamshire <ryanhamshire@live.com>
2015-03-07 11:10:50 -0800

    Added warning for outdated servers.
    For those vulnerable to piston grief due to the addition of slime
    blocks, which the Spigot team did not address in early versions of 1.8.

--- src/me/ryanhamshire/GriefPrevention/BlockEventHandler.java
+++ src/me/ryanhamshire/GriefPrevention/BlockEventHandler.java
@@ -474,54 +474,61 @@ public class BlockEventHandler implements Listener
 		//pulling up is always safe
 		if(event.getDirection() == BlockFace.UP) return;
 		
-		//don't track in worlds where claims are not enabled
-        if(!GriefPrevention.instance.claimsEnabledForWorld(event.getBlock().getWorld())) return;
-		
-		//if pistons limited to only pulling blocks which are in the same claim the piston is in
-		if(GriefPrevention.instance.config_pistonsInClaimsOnly)
+		try
 		{
-		    //if piston not in a land claim, cancel event
-		    Claim pistonClaim = this.dataStore.getClaimAt(event.getBlock().getLocation(), false, null);
-		    if(pistonClaim == null)
-		    {
-		        event.setCancelled(true);
-		        return;
-		    }
-		    
-		    for(Block movedBlock : event.getBlocks())
-		    {
-		        //if pulled block isn't in the same land claim, cancel the event
-    		    if(!pistonClaim.contains(movedBlock.getLocation(), false, false))
+    		//don't track in worlds where claims are not enabled
+            if(!GriefPrevention.instance.claimsEnabledForWorld(event.getBlock().getWorld())) return;
+    		
+    		//if pistons limited to only pulling blocks which are in the same claim the piston is in
+    		if(GriefPrevention.instance.config_pistonsInClaimsOnly)
+    		{
+    		    //if piston not in a land claim, cancel event
+    		    Claim pistonClaim = this.dataStore.getClaimAt(event.getBlock().getLocation(), false, null);
+    		    if(pistonClaim == null)
     		    {
     		        event.setCancelled(true);
     		        return;
     		    }
-		    }
-		}
-		
-		//otherwise, consider ownership of both piston and block
-		else
-		{
-		    //who owns the piston, if anyone?
-            String pistonOwnerName = "_";
-            Location pistonLocation = event.getBlock().getLocation();       
-            Claim pistonClaim = this.dataStore.getClaimAt(pistonLocation, false, null);
-            if(pistonClaim != null) pistonOwnerName = pistonClaim.getOwnerName();
-		    
-		    String movingBlockOwnerName = "_";
-    		for(Block movedBlock : event.getBlocks())
+    		    
+    		    for(Block movedBlock : event.getBlocks())
+    		    {
+    		        //if pulled block isn't in the same land claim, cancel the event
+        		    if(!pistonClaim.contains(movedBlock.getLocation(), false, false))
+        		    {
+        		        event.setCancelled(true);
+        		        return;
+        		    }
+    		    }
+    		}
+    		
+    		//otherwise, consider ownership of both piston and block
+    		else
     		{
-    		    //who owns the moving block, if anyone?
-                Claim movingBlockClaim = this.dataStore.getClaimAt(movedBlock.getLocation(), false, pistonClaim);
-        		if(movingBlockClaim != null) movingBlockOwnerName = movingBlockClaim.getOwnerName();
-        		
-        		//if there are owners for the blocks, they must be the same player
-        		//otherwise cancel the event
-        		if(!pistonOwnerName.equals(movingBlockOwnerName))
+    		    //who owns the piston, if anyone?
+                String pistonOwnerName = "_";
+                Location pistonLocation = event.getBlock().getLocation();       
+                Claim pistonClaim = this.dataStore.getClaimAt(pistonLocation, false, null);
+                if(pistonClaim != null) pistonOwnerName = pistonClaim.getOwnerName();
+    		    
+    		    String movingBlockOwnerName = "_";
+        		for(Block movedBlock : event.getBlocks())
         		{
-        			event.setCancelled(true);
+        		    //who owns the moving block, if anyone?
+                    Claim movingBlockClaim = this.dataStore.getClaimAt(movedBlock.getLocation(), false, pistonClaim);
+            		if(movingBlockClaim != null) movingBlockOwnerName = movingBlockClaim.getOwnerName();
+            		
+            		//if there are owners for the blocks, they must be the same player
+            		//otherwise cancel the event
+            		if(!pistonOwnerName.equals(movingBlockOwnerName))
+            		{
+            			event.setCancelled(true);
+            		}
         		}
     		}
+		}
+		catch(NoSuchMethodError exception)
+		{
+		    GriefPrevention.AddLogEntry("Your server is running an outdated version of 1.8 which has a griefing vulnerability.  Update your server (reruns buildtools.jar to get an updated server JAR file) to ensure playres can't steal claimed blocks using pistons.");
 		}
 	}
 	
