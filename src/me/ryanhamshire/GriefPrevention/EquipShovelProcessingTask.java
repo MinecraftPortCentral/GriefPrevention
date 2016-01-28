3e8114300a5521b461984cf39f0e084f62f4ab51
bloodshot <jdroque@gmail.com>
2016-01-28 02:06:33 -0500

    Fix shovel task sending message when switching to empty slot.

--- src/me/ryanhamshire/GriefPrevention/EquipShovelProcessingTask.java
+++ src/me/ryanhamshire/GriefPrevention/EquipShovelProcessingTask.java
@@ -41,7 +41,7 @@ public class EquipShovelProcessingTask implements Runnable {
     @Override
     public void run() {
         // if he's not holding the golden shovel anymore, do nothing
-        if (player.getItemInHand().isPresent() && !player.getItemInHand().get().getItem().getId().equals(GriefPrevention.getActiveConfig(player.getWorld()).getConfig().claim.modificationTool)) {
+        if (!player.getItemInHand().isPresent() || (!player.getItemInHand().get().getItem().getId().equals(GriefPrevention.getActiveConfig(player.getWorld()).getConfig().claim.modificationTool))) {
             return;
         }
 
