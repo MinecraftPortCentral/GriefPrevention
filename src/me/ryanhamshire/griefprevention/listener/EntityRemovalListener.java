0baa8df8a34c8f24992b11b02417df97b5968167
bloodshot <jdroque@gmail.com>
2017-02-01 01:09:42 -0500

    Fix build.

--- src/me/ryanhamshire/griefprevention/listener/EntityRemovalListener.java
+++ src/me/ryanhamshire/griefprevention/listener/EntityRemovalListener.java
@@ -68,6 +68,11 @@ public class EntityRemovalListener implements IWorldEventListener {
     }
 
     @Override
+    public void spawnParticle(int p_190570_1_, boolean p_190570_2_, boolean p_190570_3_, double p_190570_4_, double p_190570_6_, double p_190570_8_,
+            double p_190570_10_, double p_190570_12_, double p_190570_14_, int... p_190570_16_) {
+    }
+
+    @Override
     public void onEntityAdded(Entity entityIn) {
     }
 
