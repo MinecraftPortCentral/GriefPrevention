fe53f1784bb913c06d62a3cd9a2fe43a26f4d7e2
bloodshot <jdroque@gmail.com>
2018-07-06 16:54:50 -0400

    Update GP config with latest changes made by Luck.
    Before running this build, do the following
    
    1. Backup ./config/griefprevention/worlds/global.conf
    2. Delete global.conf
    3. Start server
    4. Migrate any settings you had in original global.conf back.

--- src/main/java/me/ryanhamshire/griefprevention/configuration/GriefPreventionConfig.java
+++ src/main/java/me/ryanhamshire/griefprevention/configuration/GriefPreventionConfig.java
@@ -24,9 +24,9 @@
  */
 package me.ryanhamshire.griefprevention.configuration;
 
-import com.google.common.reflect.TypeToken;
 import me.ryanhamshire.griefprevention.GriefPreventionPlugin;
 import me.ryanhamshire.griefprevention.configuration.type.ConfigBase;
+import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.ConfigurationOptions;
 import ninja.leaping.configurate.ValueType;
 import ninja.leaping.configurate.commented.CommentedConfigurationNode;
@@ -34,32 +34,46 @@ import ninja.leaping.configurate.commented.SimpleCommentedConfigurationNode;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import ninja.leaping.configurate.objectmapping.ObjectMapper;
 import ninja.leaping.configurate.objectmapping.ObjectMappingException;
-import ninja.leaping.configurate.objectmapping.serialize.TypeSerializers;
 import ninja.leaping.configurate.util.ConfigurationNodeWalker;
 import org.spongepowered.common.SpongeImpl;
-import org.spongepowered.common.util.IpSet;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Iterator;
+import java.util.Objects;
 
 public class GriefPreventionConfig<T extends ConfigBase> {
 
     private static final ConfigurationOptions LOADER_OPTIONS = ConfigurationOptions.defaults()
-            .setHeader(GriefPreventionPlugin.CONFIG_HEADER)
-            .setSerializers(TypeSerializers.getDefaultSerializers().newChild()
-                    .registerType(TypeToken.of(IpSet.class), new IpSet.IpSetSerializer())
-            );
+            .setHeader(GriefPreventionPlugin.CONFIG_HEADER);
 
-    private CommentedConfigurationNode data = SimpleCommentedConfigurationNode.root(LOADER_OPTIONS);
-
-    private final GriefPreventionConfig parent;
     private final Path path;
 
+    /**
+     * The parent configuration - values are inherited from this
+     */
+    private final GriefPreventionConfig parent;
+
+    /**
+     * The loader (mapped to a file) used to read/write the config to disk
+     */
     private HoconConfigurationLoader loader;
+
+    /**
+     * A node representation of "whats actually in the file".
+     */
+    private CommentedConfigurationNode fileData = SimpleCommentedConfigurationNode.root(LOADER_OPTIONS);
+
+    /**
+     * A node representation of {@link #fileData}, merged with the data of {@link #parent}.
+     */
+    private CommentedConfigurationNode data = SimpleCommentedConfigurationNode.root(LOADER_OPTIONS);
+
+    /**
+     * The mapper instance used to populate the config instance
+     */
     private ObjectMapper<T>.BoundInstance configMapper;
-    private T configObject;
 
     public GriefPreventionConfig(Class<T> clazz, Path path, GriefPreventionConfig parent) {
         this.parent = parent;
@@ -71,7 +85,7 @@ public class GriefPreventionConfig<T extends ConfigBase> {
                 Files.createFile(path);
             }
 
-            this.loader = HoconConfigurationLoader.builder().setPath(path).setDefaultOptions(LOADER_OPTIONS).build();
+            this.loader = HoconConfigurationLoader.builder().setPath(path).build();
             this.configMapper = ObjectMapper.forClass(clazz).bindToNew();
 
             reload();
@@ -82,79 +96,143 @@ public class GriefPreventionConfig<T extends ConfigBase> {
     }
 
     public T getConfig() {
-        return this.configObject;
+        return this.configMapper.getInstance();
     }
 
-    public void save() {
+    public boolean save() {
         try {
             // save from the mapped object --> node
-            this.configMapper.serialize(this.data.getNode(GriefPreventionPlugin.MOD_ID));
-
-            CommentedConfigurationNode saveNode = this.data.copy();
+            CommentedConfigurationNode saveNode = SimpleCommentedConfigurationNode.root(LOADER_OPTIONS);
+            this.configMapper.serialize(saveNode.getNode(GriefPreventionPlugin.MOD_ID));
 
             // before saving this config, remove any values already declared with the same value on the parent
             if (this.parent != null) {
-                Iterator<ConfigurationNodeWalker.VisitedNode<CommentedConfigurationNode>> it = ConfigurationNodeWalker.DEPTH_FIRST_POST_ORDER.walkWithPath(saveNode);
-                while (it.hasNext()) {
-                    ConfigurationNodeWalker.VisitedNode<CommentedConfigurationNode> next = it.next();
-                    CommentedConfigurationNode node = next.getNode();
-
-                    // remove empty maps
-                    if (node.hasMapChildren()) {
-                        if (node.getChildrenMap().isEmpty()) {
-                            node.setValue(null);
-                        }
-                        continue;
-                    }
-
-                    // ignore list values
-                    if (node.getParent() != null && node.getParent().getValueType() == ValueType.LIST) {
-                        continue;
-                    }
-
-                    // if the node already exists in the parent config, remove it
-                    CommentedConfigurationNode parentValue = this.parent.data.getNode(next.getPath().getArray());
-                    if (node.equals(parentValue)) {
-                        node.setValue(null);
-                    }
-                }
+                removeDuplicates(saveNode);
             }
 
+            // merge the values we need to write with the ones already declared in the file
+            saveNode.mergeValuesFrom(this.fileData);
+
+            // save the data to disk
             this.loader.save(saveNode);
+            return true;
         } catch (IOException | ObjectMappingException e) {
-            SpongeImpl.getLogger().error("Failed to save configuration", e);
+            GriefPreventionPlugin.instance.getLogger().error("Failed to save configuration", e);
+            return false;
         }
     }
 
     public void reload() {
         try {
-            // load data from the file
-            this.data = this.loader.load();
+            // load settings from file
+            CommentedConfigurationNode loadedNode = this.loader.load();
 
-            // merge with parent
+            // attempt to strip duplicate settings from the file
+            // (this only happens on the first pass of the file - see javadocs below)
+            if (cleanupConfig(loadedNode)) {
+                this.loader.save(loadedNode);
+            }
+
+            // store "what's in the file" separately in memory
+            this.fileData = loadedNode;
+
+            // make a copy of the file data
+            this.data = this.fileData.copy();
+
+            // merge with settings from parent
             if (this.parent != null) {
                 this.data.mergeValuesFrom(this.parent.data);
             }
 
             // populate the config object
-            this.configObject = this.configMapper.populate(this.data.getNode(GriefPreventionPlugin.MOD_ID));
+            populateInstance();
         } catch (Exception e) {
-            SpongeImpl.getLogger().error("Failed to load configuration", e);
+            GriefPreventionPlugin.instance.getLogger().error("Failed to load configuration", e);
+        }
+    }
+
+    private void populateInstance() throws ObjectMappingException {
+        this.configMapper.populate(this.data.getNode(GriefPreventionPlugin.MOD_ID));
+    }
+
+    /**
+     * Performs a cleanup operation on the given configuration node, taking into
+     * account the legacy 'config-enabled' setting.
+     *
+     * @param root The node to cleanup
+     * @return If the cleanup was able to occur, depending on the state of the 'config-enabled' setting
+     */
+    private boolean cleanupConfig(CommentedConfigurationNode root) {
+        // we can't strip values from the global config, there won't be any duplicates there
+        if (this.parent == null) {
+            return false;
+        }
+
+        ConfigurationNode configEnabled = root.getNode(GriefPreventionPlugin.MOD_ID, "config-enabled");
+
+        // if the node is missing, don't strip anything from the file
+        // (this ensures the migration only happens on the first pass)
+        if (configEnabled.isVirtual()) {
+            return false;
+        }
+
+        boolean enabled = configEnabled.getBoolean(true);
+
+        // remove the enabled property so migration doesn't happen again
+        configEnabled.setValue(null);
+
+        if (!enabled) {
+            // config wasn't enabled, just clear it
+            // (we don't wish to take account for any of the settings defined here)
+            root.getNode(GriefPreventionPlugin.MOD_ID).setValue(null);
+        } else {
+            // config was enabled, but we want to strip out duplicated values
+            // but keep any overrides
+            removeDuplicates(root);
+        }
+
+        return true;
+    }
+
+    /**
+     * Traverses the given {@code root} config node, removing any values which
+     * are also present and set to the same value on this configs "parent".
+     *
+     * @param root The node to process
+     */
+    private void removeDuplicates(CommentedConfigurationNode root) {
+        if (this.parent == null) {
+            throw new IllegalStateException("parent is null");
+        }
+
+        Iterator<ConfigurationNodeWalker.VisitedNode<CommentedConfigurationNode>> it = ConfigurationNodeWalker.DEPTH_FIRST_POST_ORDER.walkWithPath(root);
+        while (it.hasNext()) {
+            ConfigurationNodeWalker.VisitedNode<CommentedConfigurationNode> next = it.next();
+            CommentedConfigurationNode node = next.getNode();
+
+            // remove empty maps
+            if (node.hasMapChildren()) {
+                if (node.getChildrenMap().isEmpty()) {
+                    node.setValue(null);
+                }
+                continue;
+            }
+
+            // ignore list values
+            if (node.getParent() != null && node.getParent().getValueType() == ValueType.LIST) {
+                continue;
+            }
+
+            // if the node already exists in the parent config, remove it
+            CommentedConfigurationNode parentValue = this.parent.data.getNode(next.getPath().getArray());
+            if (Objects.equals(node.getValue(), parentValue.getValue())) {
+                node.setValue(null);
+            }
         }
     }
 
     public CommentedConfigurationNode getRootNode() {
         return this.data.getNode(GriefPreventionPlugin.MOD_ID);
-    }
-
-    public CommentedConfigurationNode getSetting(String key) {
-        if (!key.contains(".") || key.indexOf('.') == key.length() - 1) {
-            return null;
-        } else {
-            String category = key.substring(0, key.indexOf('.'));
-            String prop = key.substring(key.indexOf('.') + 1);
-            return getRootNode().getNode(category).getNode(prop);
-        }
     }
 
     public Path getPath() {
